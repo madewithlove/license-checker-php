ab8c485cc96be68bc971c3e9f57683557ec91233
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-04 10:38:55 +0100

    Add console command to get current licenses

--- src/Commands/CheckLicenses.php
+++ src/Commands/CheckLicenses.php
@@ -2,13 +2,33 @@
 
 namespace LicenseChecker\Commands;
 
+use LicenseChecker\Composer\LicenseParser;
+use LicenseChecker\Composer\LicenseRetriever;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class CheckLicenses extends Command
 {
     protected static $defaultName = 'licenses:check';
+
+    /**
+     * @var LicenseRetriever
+     */
+    private $licenseRetriever;
+
+    /**
+     * @var LicenseParser
+     */
+    private $licenseParser;
+
+    public function __construct(LicenseRetriever $licenseRetriever, LicenseParser $licenseParser)
+    {
+        parent::__construct();
+        $this->licenseRetriever = $licenseRetriever;
+        $this->licenseParser = $licenseParser;
+    }
 
     protected function configure()
     {
@@ -17,7 +37,18 @@ class CheckLicenses extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('foobar');
+        try {
+            $licenseJson = $this->licenseRetriever->getComposerLicenses(__DIR__ . '/../../');
+            $usedLicenses = $this->licenseParser->parseLicenses($licenseJson);
+            $output->writeln('Used licenses:');
+            $output->writeln('--------------');
+            foreach ($usedLicenses as $usedLicense) {
+                $output->writeln($usedLicense);
+            }
+        } catch (ProcessFailedException $e) {
+            $output->writeln($e->getMessage());
+            return 1;
+        }
 
         return 0;
     }
