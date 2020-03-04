4413515b23830f4a10ff4be5c0d80d685adc4864
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-04 20:45:50 +0100

    Pass configuration parsing and compare the used and allowed licenses

--- src/Commands/CheckLicenses.php
+++ src/Commands/CheckLicenses.php
@@ -4,10 +4,12 @@ namespace LicenseChecker\Commands;
 
 use LicenseChecker\Composer\LicenseParser;
 use LicenseChecker\Composer\LicenseRetriever;
+use LicenseChecker\Configuration\AllowedLicensesParser;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Yaml\Exception\ParseException;
 
 class CheckLicenses extends Command
 {
@@ -23,11 +25,20 @@ class CheckLicenses extends Command
      */
     private $licenseParser;
 
-    public function __construct(LicenseRetriever $licenseRetriever, LicenseParser $licenseParser)
-    {
+    /**
+     * @var AllowedLicensesParser
+     */
+    private $allowedLicensesParser;
+
+    public function __construct(
+        LicenseRetriever $licenseRetriever,
+        LicenseParser $licenseParser,
+        AllowedLicensesParser $allowedLicensesParser
+    ) {
         parent::__construct();
         $this->licenseRetriever = $licenseRetriever;
         $this->licenseParser = $licenseParser;
+        $this->allowedLicensesParser = $allowedLicensesParser;
     }
 
     protected function configure()
@@ -40,13 +51,26 @@ class CheckLicenses extends Command
         try {
             $licenseJson = $this->licenseRetriever->getComposerLicenses(__DIR__ . '/../../');
             $usedLicenses = $this->licenseParser->parseLicenses($licenseJson);
-            $output->writeln('Used licenses:');
-            $output->writeln('--------------');
-            foreach ($usedLicenses as $usedLicense) {
-                $output->writeln($usedLicense);
-            }
         } catch (ProcessFailedException $e) {
             $output->writeln($e->getMessage());
+            return 1;
+        }
+
+        try {
+            $allowedLicenses = $this->allowedLicensesParser->getAllowedLicenses();
+        } catch (ParseException $e) {
+            $output->writeln('boooh');
+            $output->writeln($e->getMessage());
+            return 1;
+        }
+
+        $notAllowedLicenses = array_diff($usedLicenses, $allowedLicenses);
+
+        if (!empty($notAllowedLicenses)) {
+            $output->writeln('The following licenses are used but not allowed:');
+            foreach ($notAllowedLicenses as $notAllowedLicense) {
+                $output->writeln('- ' . $notAllowedLicense);
+            }
             return 1;
         }
 
