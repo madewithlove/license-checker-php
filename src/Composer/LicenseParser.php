6e07fd69854295b1ef09f67e7644994aa35834e4
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-04 10:32:14 +0100

    Implement simple parser to satisfy test case

--- src/Composer/LicenseParser.php
+++ src/Composer/LicenseParser.php
@@ -7,10 +7,17 @@ use phpDocumentor\Reflection\Types\Array_;
 class LicenseParser
 {
     /**
+     * @param string $json
      * @return string[]
      */
     public function parseLicenses(string $json): array
     {
-        return [];
+        $licenses = [];
+        $decodedJson = json_decode($json, true);
+        foreach ($decodedJson['dependencies'] as $dependency) {
+            $licenses[] = $dependency['license'][0];
+        }
+
+        return array_unique($licenses);
     }
 }
