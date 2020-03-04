884fb67b347de0fe40bf4f284700674e055e8fda
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-04 10:38:24 +0100

    Add retriever to get used licenses from composer

---
+++ src/Composer/LicenseRetriever.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace LicenseChecker\Composer;
+
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class LicenseRetriever
+{
+    public function getComposerLicenses(string $composerJsonPath): string
+    {
+        $process = new Process(['composer', 'license', '-f', 'json'], $composerJsonPath);
+        $process->run();
+
+        if (!$process->isSuccessful())
+        {
+            throw new ProcessFailedException($process);
+        }
+
+        return $process->getOutput();
+    }
+}
