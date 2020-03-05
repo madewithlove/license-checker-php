104739a09070cb00a1e7179c8ac8498fe1c88998
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-05 07:29:27 +0100

    Add separate command to list used licenses

---
+++ src/Commands/ListLicenses.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace LicenseChecker\Commands;
+
+use LicenseChecker\Composer\LicenseParser;
+use LicenseChecker\Composer\LicenseRetriever;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class ListLicenses extends Command
+{
+    protected static $defaultName = 'licenses:list';
+
+    /**
+     * @var LicenseRetriever
+     */
+    private $licenseRetriever;
+
+    /**
+     * @var LicenseParser
+     */
+    private $licenseParser;
+
+    public function __construct(
+        LicenseRetriever $licenseRetriever,
+        LicenseParser $licenseParser
+    ) {
+        parent::__construct();
+        $this->licenseRetriever = $licenseRetriever;
+        $this->licenseParser = $licenseParser;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('List used licenses of composer dependencies');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $licenseJson = $this->licenseRetriever->getComposerLicenses(__DIR__ . '/../../');
+            $usedLicenses = $this->licenseParser->parseLicenses($licenseJson);
+            foreach ($usedLicenses as $usedLicense) {
+                $output->writeln($usedLicense);
+            }
+        } catch (ProcessFailedException $e) {
+            $output->writeln($e->getMessage());
+            return 1;
+        }
+
+        return 0;
+    }
+}
