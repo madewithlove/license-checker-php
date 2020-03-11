9b88337a9dede734999e4db6f97f70bf6d9fe89a
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-11 08:25:10 +0100

    Add method to write configuration file

--- src/Configuration/AllowedLicensesParser.php
+++ src/Configuration/AllowedLicensesParser.php
@@ -14,6 +14,32 @@ class AllowedLicensesParser
      */
     public function getAllowedLicenses(string $pathToConfigurationFile): array
     {
-        return Yaml::parseFile($pathToConfigurationFile . '/' . self::CONFIG_FILE_NAME);
+        return Yaml::parseFile($this->getConfigurationFilePath($pathToConfigurationFile));
+    }
+
+    /**
+     * @param string[] $allowedLicenses
+     */
+    public function writeConfiguration(array $allowedLicenses)
+    {
+        if ($this->configurationExists(getcwd())) {
+            throw new ConfigurationExists();
+        }
+        $yaml = Yaml::dump($allowedLicenses);
+        file_put_contents($this->getConfigurationFilePath(), $yaml);
+    }
+
+    private function configurationExists(string $pathToConfigurationFile): bool
+    {
+        return file_exists($this->getConfigurationFilePath($pathToConfigurationFile));
+    }
+
+    private function getConfigurationFilePath(?string $path = null): string
+    {
+        if (!$path) {
+            $path = getcwd();
+        }
+
+        return $path . '/' . self::CONFIG_FILE_NAME;
     }
 }
