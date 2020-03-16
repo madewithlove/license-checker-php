545f3d20b05a0a24746d321ae37f780859bcdda4
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-16 08:18:05 +0100

    Add simple dependency model with a one-dimensional list of sub dependencies
    All models are wrong, but this one might be useful for us :)

---
+++ src/Dependency.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace LicenseChecker;
+
+class Dependency
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string[]
+     */
+    private $subDependencies = [];
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function addDependency(string $dependency): self
+    {
+        if (!$this->hasDependency($dependency)) {
+            $this->subDependencies[] = $dependency;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDependencies(): array
+    {
+        return $this->subDependencies;
+    }
+
+    public function hasDependency(string $dependency): bool
+    {
+        return array_search($dependency, $this->getDependencies());
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
