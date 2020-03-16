3d2f1eeb28e36b7863d1abd82050762db5fb6d50
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-16 20:06:23 +0100

    Add test for dependency parser

---
+++ src/Composer/DependencyTreeTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace LicenseChecker\Composer;
+
+use LicenseChecker\Dependency;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class DependencyTreeTest extends TestCase
+{
+    /**
+     * @var DependencyTreeRetriever | MockObject
+     */
+    private $retriever;
+
+    /**
+     * @var DependencyTree
+     */
+    private $dependencyTree;
+
+    protected function setUp(): void
+    {
+        $this->retriever = $this->createMock(DependencyTreeRetriever::class);
+        $this->dependencyTree = new DependencyTree($this->retriever);
+    }
+
+    /**
+     * @test
+     */
+    public function itCanParseJsonFromComposer(): void
+    {
+        $this->retriever->method('getDependencyTree')->willReturn($this->getDependencyTree());
+
+        $dependencies = $this->dependencyTree->getDependencies();
+        $expected = [
+            (new Dependency('direct/dependency'))
+                ->addDependency('subdependency/one')
+                ->addDependency('subdependency/two')
+                ->addDependency('subdependency/three'),
+        ];
+
+        $this->assertEquals($expected, $dependencies);
+    }
+
+    private function getDependencyTree(): string
+    {
+        return '{
+            "installed": [
+                {
+                    "name": "direct/dependency",
+                    "version": "v0.1",
+                    "description": "Some direct dependency",
+                    "requires": [
+                        {
+                            "name": "subdependency/one",
+                            "version": "^1.0"
+                        },
+                        {
+                            "name": "subdependency/two",
+                            "version": "^2.0",
+                            "requires": [
+                                {
+                                    "name": "subdependency/three",
+                                    "version": "^3.0"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }';
+    }
+
+
+}
