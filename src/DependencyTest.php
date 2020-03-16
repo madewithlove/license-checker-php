62fad4815672c184c3e5361c82066f8890c0612d
Jonas Drieghe <jonasd@madewithlove.be>
2020-03-16 19:51:18 +0100

    Add tests for Dependency model

---
+++ src/DependencyTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace LicenseChecker;
+
+use PHPUnit\Framework\TestCase;
+
+class DependencyTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itKnowsItsName(): void
+    {
+        $dependency = new Dependency('foo');
+        $this->assertEquals('foo', $dependency->getName());
+    }
+
+    /**
+     * @test
+     */
+    public function itKnowsItsSubDependencies(): void
+    {
+        $expected = [
+            'bar',
+            'baz',
+        ];
+
+        $dependency = new Dependency('foo');
+        $dependency->addDependency('bar');
+        $dependency->addDependency('baz');
+
+        $this->assertEquals($expected, $dependency->getDependencies());
+        $this->assertFalse($dependency->hasDependency('foo'));
+        $this->assertTrue($dependency->hasDependency('bar'));
+        $this->assertTrue($dependency->hasDependency('baz'));
+    }
+
+    /**
+     * @test
+     */
+    public function itIgnoresDuplicateSubDependencies(): void
+    {
+        $expected = [
+            'bar',
+        ];
+
+        $dependency = new Dependency('foo');
+        $dependency->addDependency('bar');
+        $dependency->addDependency('bar');
+        $dependency->addDependency('bar');
+
+        $this->assertEquals($expected, $dependency->getDependencies());
+        $this->assertTrue($dependency->hasDependency('bar'));
+    }
+}
